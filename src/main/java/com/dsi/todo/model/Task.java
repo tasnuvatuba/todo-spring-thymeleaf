df993d6c14b8b139ab699f9d7e5c1fed015455cd
tasnuvatuba <tasnuvatuba99@gmail.com>
2024-04-08 16:38:31 +0600

    add todo entity class

---
+++ src/main/java/com/dsi/todo/model/Task.java
@@ -0,0 +1,86 @@
+package com.dsi.todo.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "task")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+    private String title;
+    private String description;
+    private Integer priority;
+    private String status;
+    private String createdAt;
+    private String updatedAt;
+
+    public Task() {
+    }
+
+    public Task(String title, String description, Integer priority, String status, String createdAt, String updatedAt) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
